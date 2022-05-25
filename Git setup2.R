df045a257779843e94d9b800060cef606aa03d50
adrearly <adreanna.early@gmail.com>
2022-05-25 07:53:44 -0600

    adding second setup file to test delete

---
+++ Git setup2.R
@@ -0,0 +1,3 @@
+usethis::use_git_config(user.name = "adrearly", user.email = "adreanna.early@gmail.com")
+credentials::set_github_pat() # You will be prompted to provide your GitHub PAT credentials::git_credential_forget() to forget credentials
+
