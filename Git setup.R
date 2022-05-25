b587524718bb995483d3aca4d44bcad77676b525
adrearly <50000793+adrearly@users.noreply.github.com>
2022-05-25 08:01:18 -0600

    Update Git setup.R
    Added new comment line 1

--- Git setup.R
+++ Git setup.R
@@ -1,3 +1,4 @@
 usethis::use_git_config(user.name = "adrearly", user.email = "adreanna.early@gmail.com")
 credentials::set_github_pat() # You will be prompted to provide your GitHub PAT credentials::git_credential_forget() to forget credentials
 
+# added new comment 1
