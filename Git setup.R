ead74e216a38eb074b03e9e5f637ad28f09b4494
adrearly <adreanna.early@gmail.com>
2022-05-25 08:14:27 -0600

    added new line in RStudio

--- Git setup.R
+++ Git setup.R
@@ -2,3 +2,4 @@ usethis::use_git_config(user.name = "adrearly", user.email = "adreanna.early@gma
 credentials::set_github_pat() # You will be prompted to provide your GitHub PAT credentials::git_credential_forget() to forget credentials
 
 # edited new comment 1 in GitHub
+# added new line in RStudio
